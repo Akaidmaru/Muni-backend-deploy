996e881aeb3b01338ab7313bfaf6a9c9d31f3f40
LuisaVargas-9 <luisavr0997@gmail.com>
2026-02-21 17:21:47 -0300

    commit inicial

---
+++ src/user/dto/update-user-dto.ts
@@ -0,0 +1,16 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateUserDto } from './create-user-dto';
+
+import { ApiPropertyOptional } from '@nestjs/swagger';
+
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiPropertyOptional({ example: 'usuario@email.com' })
+  email?: string;
+
+  @ApiPropertyOptional({ example: '+573001234567' })
+  phone?: string;
+
+  @ApiPropertyOptional({ example: 'Juan Pérez' })
+  name?: string;
+}
+
